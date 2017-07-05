ea65cc4a6878628a31d280a2f4619d5649cb59f0
stojan.milosev <stojan.milosev@yahoo.com>
2017-07-05 11:48:37 +0200

    added more css, modified javascript

---
+++ main.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("modal-image");
+var img = document.getElementById("img");
+var modalContent = document.getElementById("img-1");
+var close = document.getElementById("close-image");
+var caption = document.getElementsByClassName("caption");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalContent.src = this.src;
+    caption.innerHTML = this.alt;
+};
+
+var close = document.getElementById("close-img");
+
+close.onclick = function(){
+    modal.style.display = "none";
+};
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+};
